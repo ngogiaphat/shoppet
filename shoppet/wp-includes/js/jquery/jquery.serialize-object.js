a54f91017673c8e03b85e4aa903d703bb8b59957
ngogiaphat <ngogiaphat0802@gmail.com>
2023-05-24 21:02:52 +0700

    ...

--- shoppet/wp-includes/js/jquery/jquery.serialize-object.js
+++ shoppet/wp-includes/js/jquery/jquery.serialize-object.js
@@ -5,27 +5,19 @@
  * Copyright (c) 2010 "Cowboy" Ben Alman
  * Dual licensed under the MIT and GPL licenses.
  * http://benalman.com/about/license/
- */
-
-// Whereas .serializeArray() serializes a form into an array, .serializeObject()
-// serializes a form into an (arguably more useful) object.
+*/
+// Whereas .serializeArray() serializes a form into an array, .serializeObject() serializes a form into an (arguably more useful) object.
 
 (function($,undefined){
   '$:nomunge'; // Used by YUI compressor.
-  
   $.fn.serializeObject = function(){
-    var obj = {};
-    
-    $.each( this.serializeArray(), function(i,o){
-      var n = o.name,
-        v = o.value;
-        
-        obj[n] = obj[n] === undefined ? v
-          : $.isArray( obj[n] ) ? obj[n].concat( v )
-          : [ obj[n], v ];
+    var obj = {
+
+    };
+    $.each(this.serializeArray(), function(i, o){
+      var n = o.name, v = o.value;
+        obj[n] = obj[n] === undefined ? v : $.isArray(obj[n]) ? obj[n].concat(v) : [obj[n], v];
     });
-    
     return obj;
   };
-  
 })(jQuery);
